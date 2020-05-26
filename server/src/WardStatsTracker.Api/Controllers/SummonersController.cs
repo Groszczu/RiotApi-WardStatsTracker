c9a3def184e3ea6e112033c600c8934a42cf5976
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-05-26 23:20:55 +0200

    Add includeLeagues flag to summoners controller

--- server/src/WardStatsTracker.Api/Controllers/SummonersController.cs
+++ server/src/WardStatsTracker.Api/Controllers/SummonersController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Refit;
 using WardStatsTracker.Core.Interfaces;
 using WardStatsTracker.Core.Models;
 using WardStatsTracker.Core.Parameters;
@@ -19,16 +22,36 @@ namespace WardStatsTracker.Api.Controllers
         }
 
         [HttpGet("{summonerName}")]
-        public async Task<ActionResult<SummonerModel>> GetSummonerByName(string platformId, string summonerName, [FromQuery] bool includeMatches = false)
+        public async Task<ActionResult<SummonerModel>> GetSummonerByName(string platformId, string summonerName, 
+            [FromQuery] bool includeMatches = true, 
+            [FromQuery] bool includeLeagues = true)
         {
-            var summoner = await _riotService.GetSummoner(platformId, summonerName);
-            if (summoner.AccountId == null)
+            SummonerModel summoner;
+            try
+            {
+                summoner = await _riotService.GetSummoner(platformId, summonerName);
+            }
+            catch (ApiException)
+            {
                 return NotFound("Summoner with given name could not be find");
+            }
             
-            if (!includeMatches)
-                return summoner;
+            if (includeMatches)
+            {
+                try
+                {
+                    summoner.Matches = await _riotService.GetMatchesByAccount(platformId, summoner.AccountId!);
+                }
+                catch (ApiException)
+                {
+                    summoner.Matches = new List<MatchOverviewModel>();
+                }
+            }
 
-            summoner.Matches = await _riotService.GetMatchesByAccount(platformId, summoner.AccountId);
+            if (includeLeagues)
+            {
+                summoner.Leagues = await _riotService.GetLeaguesBySummoner(platformId, summoner.Id!);
+            }
 
             return summoner;
         }
--- server/src/WardStatsTracker.Core/Models/SummonerModel.cs
+++ server/src/WardStatsTracker.Core/Models/SummonerModel.cs
@@ -9,6 +9,8 @@ namespace WardStatsTracker.Core.Models
         public string? AccountId { get; set; }
         public string? Name { get; set; }
         public int SummonerLevel { get; set; }
+        public int ProfileIconId { get; set; }
         public IList<MatchOverviewModel>? Matches { get; set; } = null;
+        public IList<LeagueEntryModel>? Leagues { get; set; } = null;
     }
 }
